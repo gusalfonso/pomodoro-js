deaedf143b5596fb4f2a450a1caa99d9378cd251
Gustavo Alfonso <alfonsgustavo@gmail.com>
2024-07-18 16:44:59 -0300

    primerentregable

--- main.js
+++ main.js
@@ -2,24 +2,44 @@
 const playBtn = document.querySelector(".play-btn");
 const pauseBtn = document.querySelector(".pause-btn");
 const timerTime = document.querySelector(".timer-time");
-const workDurationInput = 20;
-const restDurationInput = 10;
 const circle = document.getElementById("timer-fg");
 const configBtn = document.querySelector(".config-btn");
 const configBtnSet = document.querySelector(".config-btn-settings");
 const pomodoroContainer = document.getElementById("pomodoro-container");
 const configContainer = document.getElementById("pomodoro-container-settings");
+const laps = document.getElementById("laps");
+const body = document.querySelector("body");
 
-let defaultWorkTime = 20;
-let defaultRestTime = 10;
-let workDuration = workDurationInput;
-let restDuration = restDurationInput;
+const workDurationInput = document.getElementById("work-input");
+const restDurationInput = document.getElementById("rest-input");
+
+let workDuration = workDurationInput.value * 60;
+let restDuration = restDurationInput.value * 60;
+
+workDurationInput.addEventListener("change", () => {
+  workDuration = parseInt(workDurationInput.value * 60);
+  if (isWorking) {
+    remainingTime = workDuration;
+    refreshProgress(remainingTime);
+  }
+});
+
+restDurationInput.addEventListener("change", () => {
+  restDuration = parseInt(restDurationInput.value * 60);
+  if (!isWorking) {
+    remainingTime = restDuration;
+    refreshProgress(remainingTime);
+  }
+});
+
 let remainingTime = workDuration;
 let isRunning = false;
 let isWorking = true;
 let intervalId;
+let lap = 0;
 
 timerTime.innerHTML = formattedTime(remainingTime);
+laps.innerHTML = lap;
 
 //Formatting time:
 function formattedTime(time) {
@@ -52,7 +72,10 @@ function play() {
   playBtn.setAttribute("disabled", "");
   pauseBtn.removeAttribute("disabled", "");
   isRunning = true;
-  let time = 0;
+
+  const workAudio = new Audio("/sound1.wav");
+  const restAudio = new Audio("/sound2.wav");
+  let playSound;
 
   if (!intervalId) {
     intervalId = setInterval(() => {
@@ -61,7 +84,17 @@ function play() {
         if (remainingTime <= 0) {
           isWorking = !isWorking;
           remainingTime = isWorking ? workDuration : restDuration;
+          if (!isWorking) {
+            lap++;
+            laps.innerHTML = lap;
+            body.classList.add("rest-time");
+            restAudio.play();
+          } else {
+            body.classList.remove("rest-time");
+            workAudio.play();
+          }
         }
+
         refreshProgress(remainingTime);
       }
     }, 1000);
@@ -86,9 +119,15 @@ pauseBtn.addEventListener("click", () => {
 configBtn.addEventListener("click", () => {
   pomodoroContainer.style.display = "none";
   configContainer.style.display = "block";
+  lap = 0;
+  isRunning = false;
+  body.classList.remove("rest-time");
+  playBtn.removeAttribute("disabled", "");
+  pauseBtn.setAttribute("disabled", "");
 });
 
 configBtnSet.addEventListener("click", () => {
   pomodoroContainer.style.display = "block";
   configContainer.style.display = "none";
+  lap = 0;
 });
